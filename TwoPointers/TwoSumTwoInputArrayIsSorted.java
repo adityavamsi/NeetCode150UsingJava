6bb2093a70b3d0b262a28023216e00d7d1e67912
Aditya Vamsi <chvamsiaditya33@gmail.com>
2025-06-23 19:44:45 +0530

    Done

---
+++ TwoPointers/TwoSumTwoInputArrayIsSorted.java
@@ -0,0 +1,41 @@
+class Question{
+    // LC - 167
+    /*
+    Given an array of integers numbers that is sorted in non-decreasing order.
+    Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
+
+    There will always be exactly one valid solution.
+
+    Your solution must use
+
+    O(1) additional space.
+
+    Example 1:
+
+    Input: numbers = [1,2,3,4], target = 3
+
+    Output: [1,2]
+    Explanation:
+    The sum of 1 and 2 is 3. Since we are assuming a 1-indexed array, index1 = 1, index2 = 2. We return [1, 2].
+
+    Constraints:
+
+    2 <= numbers.length <= 1000
+    -1000 <= numbers[i] <= 1000
+    -1000 <= target <= 1000
+
+    */
+}
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int s=0,e=numbers.length-1;
+        while(s<e){
+            int sum = numbers[s]+numbers[e];
+            if(sum>target) e--;
+            else if(sum<target) s++;
+            else break;
+        }
+        return new int[]{s+1,e+1};
+    }
+}
