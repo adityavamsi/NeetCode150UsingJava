e8f33c07c62d50c413f5536f8048388ad1c36e30
Aditya Vamsi <chvamsiaditya33@gmail.com>
2025-06-26 18:29:21 +0530

    Done

--- SlidingWindow/LongestRepeatingCharactersReplacement.java
+++ SlidingWindow/LongestRepeatingCharactersReplacement.java
@@ -1,42 +1,44 @@
 class Question{
     //LC - 424
     /*
-    * Given a string s, find the length of the longest substring without duplicate characters.
-    A substring is a contiguous sequence of characters within a string.
+    You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+
+    After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
 
     Example 1:
 
-    Input: s = "zxyzxyz"
-    Output: 3
-    Explanation: The string "xyz" is the longest without duplicate characters.
+    Input: s = "XYYX", k = 2
+    Output: 4
+    Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
 
     Example 2:
 
-    Input: s = "xxxx"
-    Output: 1
+    Input: s = "AAABABB", k = 1
+    Output: 5
 
     Constraints:
 
-    0 <= s.length <= 1000
-    s may consist of printable ASCII characters.
+    1 <= s.length <= 1000
+    0 <= k <= s.length
 */
 }
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int characterReplacement(String s, int k) {
+        int n=s.length(),maxf=0,maxl=0;
         Map<Character,Integer> mp = new HashMap();
-        int n = s.length();
-        int i=0,j=0,c=0;
-        while(j<n){
+        int i=0;
+        for(int j=0;j<n;j++){
             char ch = s.charAt(j);
-            if(mp.containsKey(ch)){
-                if(mp.get(ch)>=i) i=mp.get(ch)+1;
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            maxf  = Math.max(maxf, mp.get(ch));
+            while((j-i)+1 - maxf >k){
+                mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)-1);
+                i++;
             }
-            c = Math.max(c,j-i+1);
-            mp.put(ch,j);
-            j++;
+            maxl = Math.max(maxl, j-i+1);
         }
-        return c;
+        return maxl;
 
     }
 }
